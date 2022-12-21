318152571662767bdffd103d17af530aa0425feb
vmehta <119400570+vmehta58740@users.noreply.github.com>
2022-12-21 13:21:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit
 import pandas
-import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 streamlit.title('Hello GitHub and stremlit')
 streamlit.header('Breakfast Menu')
